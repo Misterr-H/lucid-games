1800f34ba75348dab1ec7b090c1359f06da41014
Himanshu Saini <hs913271@gmail.com>
2024-06-18 23:09:16 +0530

    navbar sidebar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
+import { useRouter } from "next/router";
 import "../app/globals.css";
+import Sidebar from "@/components/SideBar";
+import NavBar from "@/components/NavBar";
 
 const MyApp = ({ Component, pageProps }: any) => {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const { route } = router;
+
+  const shouldRenderSidebar = route !== "/login" && route !== "/register";
+  const shouldRenderNavBar = route !== "/login" && route !== "/register";
+
+  return (
+    <div className="flex h-screen">
+      {shouldRenderSidebar && <Sidebar />}
+      <div className="flex flex-col w-full">
+        {shouldRenderNavBar && <NavBar />}
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 };
 
 export default MyApp;
